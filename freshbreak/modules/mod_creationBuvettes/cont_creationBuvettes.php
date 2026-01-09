70b033a1378c53736f860769833d2124f3c3996d
Lemartes <mariustissotderbier@outlook.fr>
2026-01-09 15:32:35 +0100

    Nettoyage

--- freshbreak/modules/mod_creationBuvettes/cont_creationBuvettes.php
+++ freshbreak/modules/mod_creationBuvettes/cont_creationBuvettes.php
@@ -6,19 +6,10 @@ require_once('modele_creationBuvettes.php');
 class ContCreationBuvettes {
     private $vue;
     private $modele;
-    public function __construct() {
-        $this->vue = new VueCreationBuvettes();
-        $this->modele = new ModeleCreationBuvettes();
-
-        // Démarre la session si ce n’est pas déjà fait
-        if (session_status() === PHP_SESSION_NONE) {
-            session_start();
-        }
-
     public function __construct()
     {
-        $this->vue = new vue_creationBuvettes();
-        $this->modele = new modele_creationBuvettes();
+        $this->vue = new VueCreationBuvettes();
+        $this->modele = new ModeleCreationBuvettes();
     }
 
     public function form_ajout_bar()
